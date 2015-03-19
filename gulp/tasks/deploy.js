4a1939d943d66191f859a716ff13e0c0c5644d25
Michael Haase <darkstar@gernox.de>
2015-03-19 14:34:38 +0100

    Set permissions to 0600 for deployment key

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -10,5 +10,6 @@ gulp.task('deploy', [ 'prod' ], shell.task([
     'cp -pR ' + config.dist.root + '/* /tmp/deploy/opt/ccproject-frontend',
     'rm -f *.deb',
     'fpm -s dir -t deb -C /tmp/deploy --name ccproject-frontend --version 0.0.1 --iteration build-$TRAVIS_BUILD_NUMBER .',
+    'chmod 0600 deploy_key',
     'scp -i deploy_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null *.deb deployment@puppet.cc.gernox.de:/tmp/'
 ]));
